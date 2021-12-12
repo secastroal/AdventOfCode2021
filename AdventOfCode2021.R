c307374fb0acdb3eb2e73154e1bf97b4efb79214
Sebastian Castro <secastroal@gmail.com>
2021-12-12 18:35:22 +0100

    Day 6 part 1 and 2.

--- AdventOfCode2021.R
+++ AdventOfCode2021.R
@@ -425,4 +425,126 @@ result
 
 rm(list = ls())
 
+# Challenge #6.1 ----
+
+# The sea floor is getting steeper. Maybe the sleigh keys got carried this way?
+# A massive school of glowing lanternfish swims past. They must spawn quickly to
+# reach such large numbers - maybe exponentially quickly? You should model their 
+# growth rate to be sure.
+# Although you know nothing about this specific species of lanternfish, you make 
+# some guesses about their attributes. Surely, each lanternfish creates a new 
+# lanternfish once every 7 days.
+# However, this process isn't necessarily synchronized between every lanternfish 
+# - one lanternfish might have 2 days left until it creates another lanternfish, 
+# while another might have 4. So, you can model each fish as a single number 
+# that represents the number of days until it creates a new lanternfish.
+# Furthermore, you reason, a new lanternfish would surely need slightly longer 
+# before it's capable of producing more lanternfish: two more days for its first 
+# cycle.
+# So, suppose you have a lanternfish with an internal timer value of 3:
+#     After one day, its internal timer would become 2.
+#     After another day, its internal timer would become 1.
+#     After another day, its internal timer would become 0.
+#     After another day, its internal timer would reset to 6, and it would 
+#         create a new lanternfish with an internal timer of 8.
+#     After another day, the first lanternfish would have an internal timer of 
+#         5, and the second lanternfish would have an internal timer of 7.
+# A lanternfish that creates a new fish resets its timer to 6, not 7 (because 0 
+# is included as a valid timer value). The new lanternfish starts with an 
+# internal timer of 8 and does not start counting down until the next day.
+# Realizing what you're trying to do, the submarine automatically produces a 
+# list of the ages of several hundred nearby lanternfish (your puzzle input). 
+# For example, suppose you were given the following list:
+# 3,4,3,1,2
+# This list means that the first fish has an internal timer of 3, the second 
+# fish has an internal timer of 4, and so on until the fifth fish, which has an 
+# internal timer of 2. Simulating these fish over several days would proceed as 
+# follows:
+# Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each 
+# other number decreases by 1 if it was present at the start of the day.
+# In this example, after 18 days, there are a total of 26 fish. After 80 days, 
+# there would be a total of 5934.
+# Find a way to simulate lanternfish. How many lanternfish would there be after 
+# 80 days?
+
+lanternfish <- read.table("inputs/input_d06.txt", sep = ",")
+lanternfish <- c(as.matrix(lanternfish))
+
+lf_pop      <- lanternfish
+days        <- 80
+
+i <-  0
+repeat {
+  i <- i + 1
+  if (i == days) {
+    break
+  }
+  lf_pop              <- lf_pop - 1
+  new_fish            <- sum(lf_pop == 0)
+  lf_pop[lf_pop == 0] <- 7
+  lf_pop              <- c(lf_pop, rep(9, new_fish))
+}
+rm(new_fish, i)
+
+result <- length(lf_pop)
+result
+
+rm(days, lf_pop, result)
+
+# Challenge 6.2 ----
+
+# Suppose the lanternfish live forever and have unlimited food and space. Would 
+# they take over the entire ocean?
+# After 256 days in the example above, there would be a total of 26984457539 
+# lanternfish!
+# How many lanternfish would there be after 256 days?
+
+
+#!# Don't run #!#
+#!# While this probably gives the result, it requires an obscene amount of RAM.
+lf_pop      <- lanternfish
+days        <- 256
+
+i <-  0
+repeat {
+  i <- i + 1
+  if (i == days) {
+    break
+  }
+  lf_pop              <- lf_pop - 1
+  new_fish            <- sum(lf_pop == 0)
+  lf_pop[lf_pop == 0] <- 7
+  lf_pop              <- c(lf_pop, rep(9, new_fish))
+}
+rm(new_fish, i)
+
+result <- length(lf_pop)
+result
+
+#!# I checked some tips in Reddit. The trick is to work with groups of fish 
+#!# instead of with each fish at a time.
+
+lanternfish <- factor(lanternfish, levels = 0:8)
+
+lf_pop <- as.numeric(table(lanternfish))
+days   <- 256
+
+i <-  0
+repeat {
+  i <- i + 1
+  tmp <- lf_pop[c(2:9)]
+  tmp[7] <- tmp[7] + lf_pop[1]
+  lf_pop <- c(tmp, lf_pop[1])
+  lf_pop
+  if (i == days) {
+    break
+  }
+}
+
+result <- sum(lf_pop)
+sprintf("%1.0f", result)
+
+rm(list = ls)
+
+
 # End ----
