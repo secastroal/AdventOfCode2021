72c9cf5188352352add13617c20477fa163b0120
Sebastian Castro <secastroal@gmail.com>
2021-12-12 16:04:28 +0100

    Day 5 part 1 and 2.

--- AdventOfCode2021.R
+++ AdventOfCode2021.R
@@ -333,4 +333,96 @@ result
 
 rm(list = ls())
 
+# Challenge 5.1 ----
+
+# You come across a field of hydrothermal vents on the ocean floor! These vents 
+# constantly produce large, opaque clouds, so it would be best to avoid them if 
+# possible.
+# They tend to form in lines; the submarine helpfully produces a list of nearby 
+# lines of vents (your puzzle input) for you to review. For example:
+# Each line of vents is given as a line segment in the format x1,y1 -> x2,y2 
+# where x1,y1 are the coordinates of one end the line segment and x2,y2 are the 
+# coordinates of the other end. These line segments include the points at both 
+# ends. In other words:
+#   An entry like 1,1 -> 1,3 covers points 1,1, 1,2, and 1,3.
+# An entry like 9,7 -> 7,7 covers points 9,7, 8,7, and 7,7.
+# For now, only consider horizontal and vertical lines: lines where either
+# x1 = x2 or y1 = y2.
+# So, the horizontal and vertical lines from the above list would produce the 
+# following diagram:
+# In this diagram, the top left corner is 0,0 and the bottom right corner is 9,9. 
+# Each position is shown as the number of lines which cover that point or . if 
+# no line covers that point. The top-left pair of 1s, for example, comes from 2,2 -> 2,1; the very bottom row is formed by the overlapping lines 0,9 -> 5,9 and 0,9 -> 2,9.
+# To avoid the most dangerous areas, you need to determine the number of points 
+# where at least two lines overlap. In the above example, this is anywhere in 
+# the diagram with a 2 or larger - a total of 5 points.
+# Consider only horizontal and vertical lines. At how many points do at least 
+# two lines overlap?
+tmp   <- tempfile()
+writeLines(gsub(" -> ", ",", readLines("inputs/input_d05.txt")), con = tmp)
+vents <- read.table(tmp, sep = ",")
+unlink(tmp)
+rm(tmp)
+
+# Select horizontal and vertical lines.
+
+hvlines <- vents[, 1] == vents[, 3] | vents[, 2] == vents[, 4]
+
+ventshv <- vents[hvlines, ]
+n_vents <- nrow(ventshv)
+maxvent <- max(ventshv)
+
+field     <- matrix(0, maxvent, maxvent)
+
+for (i in 1:n_vents){
+  field_tmp <- matrix(0, maxvent, maxvent)
+  if (ventshv[i, 1] == ventshv[i, 3]) {
+    field_tmp[ventshv[i, 2]:ventshv[i, 4], ventshv[i, 1]] <- 1
+  } else {
+    field_tmp[ventshv[i, 2], ventshv[i, 1]:ventshv[i, 3]] <- 1
+  }
+  field <- field + field_tmp
+}
+rm(i, field_tmp)
+
+result <- sum(field >= 2)
+result
+
+rm(field, ventshv, hvlines, maxvent, n_vents, result)
+
+# Challenge #5.2 ----
+
+# Unfortunately, considering only horizontal and vertical lines doesn't give you 
+# the full picture; you need to also consider diagonal lines.
+# Because of the limits of the hydrothermal vent mapping system, the lines in 
+# your list will only ever be horizontal, vertical, or a diagonal line at 
+# exactly 45 degrees. In other words:
+# An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+# An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+# Considering all lines from the above example would now produce the following 
+# diagram:
+# You still need to determine the number of points where at least two lines 
+# overlap. In the above example, this is still anywhere in the diagram with a 2 
+# or larger - now a total of 12 points.
+# Consider all of the lines. At how many points do at least two lines overlap?
+
+n_vents <- nrow(vents)
+maxvent <- max(vents)
+
+field     <- matrix(0, maxvent, maxvent)
+
+for (i in 1:n_vents){
+  field_tmp   <- matrix(0, maxvent, maxvent)
+  coordinates <- cbind(vents[i, 2]:vents[i, 4], 
+                       vents[i, 1]:vents[i, 3])
+  field_tmp[coordinates] <- 1
+  field       <- field + field_tmp
+}
+rm(i, field_tmp, coordinates)
+
+result <- sum(field >= 2)
+result
+
+rm(list = ls())
+
 # End ----
